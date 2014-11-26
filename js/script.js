a7f3c9327b7feb6742e0c0c4e4ad59cd31007be3
Matt Atkins <matt@redemptionmedia.co.uk>
2014-11-26 13:36:37 +0000

    Update styles and replace help link

--- js/script.js
+++ js/script.js
@@ -7,4 +7,13 @@ var link = document.createElement('link');
 link.href= chrome.extension.getURL('css/styles.css');
 link.rel = 'stylesheet';
 link.type = 'text/css';
-document.getElementsByTagName('iframe')[0].contentDocument.head.appendChild(link);
+var iframeDocument = document.getElementsByTagName('iframe')[0].contentDocument;
+iframeDocument.head.appendChild(link);
+// Change help link destination
+setTimeout(function(){
+  var helpLink = iframeDocument.getElementById(':1.he');
+  var nav = helpLink.parentNode;
+  nav.removeChild(helpLink);
+  var newHelpLink = "<a href='http://richwells.me/better-google-tasks/' target='_blank' class='goog-flat-button w goog-inline-block' role='button' aria-hidden='true' style='text-decoration:none'>Help</a>";
+  nav.innerHTML = newHelpLink + nav.innerHTML;
+},3000);
